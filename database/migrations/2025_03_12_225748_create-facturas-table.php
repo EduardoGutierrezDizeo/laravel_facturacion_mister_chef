70c95334b0a1fe1b7a7c7921d14bf9bbabdf9620
EduardoGutierrezDizeo <edojose1518@gmail.com>
2025-03-16 23:17:00 -0500

    errores ortograficos y de sintaxis en migraciones corregidos

--- database/migrations/2025_03_12_225748_create-facturas-table.php
+++ database/migrations/2025_03_12_225748_create-facturas-table.php
@@ -14,12 +14,12 @@ return new class extends Migration
         Schema::create('facturas', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('id_cliente');
-            $table->string('name');
-            $table->string('phone');
-            $table->datetime('date')->nullable();
+            $table->string('nombre');
+            $table->string('telefono');
+            $table->datetime('fecha')->nullable();
             $table->decimal('total', 10 , 2);
             $table->boolean('status');
-            $table->string('registerBy');
+            $table->string('registradoPor');
             $table->timestamp('created_at')->nullable(); 
             $table->timestamp('updated_at')->nullable();
             $table->foreign('id_cliente')->references('id')->on('clientes');
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExist('facturas');
+        Schema::dropIfExists('facturas');
     }
 };
